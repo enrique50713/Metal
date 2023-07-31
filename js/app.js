045c8d7a9cb3690095d2cc6ab87db7121eb37d0f
Secundino E. Mendoza P <131404928+enrique50713@users.noreply.github.com>
2023-07-31 08:54:08 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,22 @@
+let items = document.querySelectorAll('.content');
+items.forEach(item => {
+item.addEventListener('mousemove', (e) => {
+    //
+    let positionPx = e.x - item.getBoundingClientRect().left;
+
+    let positionX = (positionPx / item.offsetWidth)*100;
+
+    let positionPy = e.y - item.getBoundingClientRect().top;
+
+    let positionY = (positionPy / item.offsetHeight)*100;
+
+    item.style.setProperty('--rX', (0.5)*(90 - positionY) + 'deg');
+    item.style.setProperty('--rY', -(0.5)*(90 - positionX) + 'deg');
+
+})
+item.addEventListener('mouseout', () =>{
+    item.style.setProperty('--rX', '0deg');
+    item.style.setProperty('--rY', '0deg');
+})
+
+})
---
+++ js/gale.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function (){
+    let imagenes =[
+        {img:'img/Aguadulce Casa Club.png'},
+        {img:'img/Canopies Smithsonian.png'},
+        {img:'img/Casa Atletas.png'},
+        {img:'img/Casa Atletas.png'},
+        {img:'img/El Cope.png'},
+
+        {img:'img/Gimnasio La Basita.png'},
+        {img:'img/Graderias Aguadulce.png'},
+        {img:'img/Letrero Calle 50.png'},
+        {img:'img/Mienera.png'},
+        {img:'img/Mr Precio Algarrobos.png'},
+
+        {img:'img/Pontoons.png'},
+        {img:'img/pro1.png'},
+        {img:'img/pro3.png'},
+        {img:'img/pro2'},
+        {img:'img/Universidad de Panama.png'}
+    ]
+    let contador = 0
+    const contenedor = document.querySelector('.slideshow')
+    const overlay = document.querySelector('.overlay')
+    const galery_img = document.querySelectorAll('.galery img')
+    const img_slideshow = document.querySelector('.slideshow img')
+
+    
+})
